96f513474b407902d3750b26a2c1cbbe110ddcd5
mim2041 <suraiyamim338@gmail.com>
2023-03-01 11:07:38 +0600

    practicing API

---
+++ js/pp1.js
@@ -0,0 +1,33 @@
+const person = {
+    found: 2,
+    message: 'Found 2 persons',
+    result: [
+        {
+            name: {
+                common: 'John',
+                fullName: ['John', 'Doe']
+            },
+            age: 32,
+            isMale: false,
+            address: {
+                street: '13/A St Joseph',
+                house: 10
+            },
+        },
+        {
+            name: {
+                common: 'Humayoun',
+                fullName: ['Humayoun', 'Kabir']
+            },
+            age: 33,
+            isMale: false,
+            address: {
+                street: '13/A St Lucia',
+                house: 11
+            },
+        },
+    ]
+};
+
+console.log(person.result[0].name.common);
+console.log(person.result[0].age);
